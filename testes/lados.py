7291cafdb990f66b337012a41d94962f525e1619
Gabs <gabrielsilvaf@protonmail.com>
2022-06-09 22:03:42 -0300

    poo & testes

---
+++ testes/lados.py
@@ -0,0 +1,15 @@
+class Triangulo:
+    def __init__(self, lado_a, lado_b, lado_c):
+        self.a = lado_a
+        self.b = lado_b
+        self.c = lado_c
+
+    def tipo_lado(self):
+        if self.a == self.b == self.c :
+            return 'equilátero'
+        elif self.a != self.b and self.b != self.c and self.a != self.c:
+            return 'escaleno'
+        else:
+            return 'isósceles'
+
+t = Triangulo(0, 0, 0)
---
+++ testes/modulo.py
@@ -0,0 +1,20 @@
+import pytest
+
+import Bhaskara
+
+class TestBhaskara:
+
+    @pytest.fixture()  # Guarda o objeto antes declarado
+    def b(self):
+        return Bhaskara.Bhaskara()
+
+    def testa_raiz(self, b):
+        assert b.calcula_raiz(1, 0, 0) == (1, 0)
+
+    def testa_duas_raizes(self, b):
+        assert b.calcula_raiz(1, -5, 6) == (2, 3, 2)
+
+    def testa_zero_raizes(self, b):
+        assert b.calcula_raiz(10, 10, 10) == 0
+
+
